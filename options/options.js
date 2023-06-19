0c3110e001a48f4e6e03df083b4a70455e41e00e
Crowdbotics <engineering@crowdbotics.com>
2023-06-19 13:39:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-wave-dev-92993",
+  url: "https://black-wave-dev-92993.botics.co",
+  api: "https://black-wave-dev-92993.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
